7c708ceca1e8058219c7d8fb6af189635a1ac0ae
Jinesh Mehta <mehta.jin@northeastern.edu>
2023-07-09 17:37:25 -0400

    removed files added by mistake and also fixed constants

--- constants.py
+++ constants.py
@@ -1,10 +1,5 @@
 import os
 
-# Slack Web Hooks
-SLACK_JOB_NOTIFICATION_WEBHOOK = "https://hooks.slack.com/services/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"
-SLACK_ERROR_NOTIFICATION_WEBHOOK = "https://hooks.slack.com/services/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"
-SLACK_DEPLOYMENT_NOTIFICATION_WEBHOOK = "https://hooks.slack.com/services/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"
-
 # Known Data
 DATA_FOLDER = '/home/jineshmehta/new-jobs-notifier/data'
 COMPANY_NAMES_CSV = os.path.join(DATA_FOLDER, 'company_data.csv')
